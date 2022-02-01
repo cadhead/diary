194e52727f224c6f78aa59e75783c7caa7da01fe
cadhead <nekorides@gmail.com>
2022-02-01 06:39:55 +0300

    feat(pages/new): post preview

--- pages/post/new.tsx
+++ pages/post/new.tsx
@@ -1,6 +1,8 @@
 import Container from "components/container";
 import Layout from "components/layout";
+import Modal from "components/modal";
 import Nav from "components/nav";
+import { createMarkUp } from "lib/mdRenderer";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { FormEvent, useEffect, useState } from "react";
@@ -9,9 +11,11 @@ export default function NewPost() {
   const router = useRouter();
   const [state, setState] = useState({
     title: "",
-    content: ""
+    content: "",
+    showPreview: false
   });
   const [error, setError] = useState("");
+  const [modalState, setModelState] = useState(false);
 
   useEffect(() => {
     const cache = localStorage.getItem("postCache");
@@ -26,6 +30,8 @@ export default function NewPost() {
   useEffect(() => {
     localStorage.setItem("postCache", JSON.stringify({ ...state }));
   }, [state])
+
+  const modalOpen = () => setModelState(true);
 
   const postCreate = async (event: FormEvent) => {
     event.preventDefault();
@@ -60,6 +66,9 @@ export default function NewPost() {
       <Head>
         <title>Create new post - Diary</title>
       </Head>
+      <Modal onClose={() => setModelState(false)} title="Post preview" isOpen={modalState}>
+        <p dangerouslySetInnerHTML={createMarkUp(state.content)} />
+      </Modal>
       <Container>
         <Nav />
         {error ? <div className="text-red-400">{error}</div> : null}
@@ -67,6 +76,7 @@ export default function NewPost() {
           <input value={state.title} onChange={onFieldChange} type="text" name="title" placeholder="Title" className="block w-2/3 h-16 px-4 py-3 mt-8 mb-3 border rounded appearance-none bg-grey-lighter text-grey-darker" />
           <textarea value={state.content} onChange={onFieldChange} name="content" placeholder="Write smth" className="block w-full px-4 py-3 mt-8 mb-3 border rounded appearance-none h-3/6 bg-grey-lighter text-grey-darker border-grey-lighter"></textarea>
           <button className="px-4 py-2 mt-5 mb-5 font-bold text-white bg-indigo-900 rounded shadow hover:bg-indigo-800" type="submit">Create new post</button>
+          <button onClick={modalOpen} className="px-4 py-2 mt-5 mb-5 ml-1 font-bold text-white bg-gray-900 rounded shadow hover:bg-gray-800" type="button">Preview</button>
         </form>
       </Container>
     </Layout>
