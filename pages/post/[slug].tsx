08991fb01c93005912155678e1ba5cfafbe73fa2
cadhead <nekorides@gmail.com>
2022-02-01 06:39:05 +0300

    refactor: move md renderer to separate file

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -5,33 +5,9 @@ import Head from "next/head";
 import { ReactElement } from "react";
 import { useRouter } from "next/router";
 import ErrorPage from 'next/error'
-import { marked } from "marked";
 import formatedDate from "lib/formatedDate";
 import Nav from "components/nav";
-
-
-const renderer = new marked.Renderer();
-renderer.heading = (text, level): string => {
-  switch (level) {
-    case 2: {
-      return `<h2 class="text-lg font-bold mt-5 text-violet-900">${text}</h2>`;
-    }
-      
-    default: return `<h${level}>${text}</h${level}>`;
-  }
-}
-
-renderer.list = (body): string => {
-  return `<ul class="list-disc list-inside mx-5 mt-1">${body}</ul>`
-}
-
-renderer.link = (href, title, text) => {
-  return `<a href="${href}" class="text-blue-400 hover:underline hover:text-blue-500" ${title ? `title="${title}` : ""}>${text}</a>`
-}
-
-const createMarkUp = (val: string) => {
-  return { __html: marked(val, { renderer: renderer }) }
-}
+import { createMarkUp } from "lib/mdRenderer";
 
 export default function Post(post: IPost): ReactElement {
   const router = useRouter()
