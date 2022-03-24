e3418dac582e288d42856f7c7673a38f7422dcd7
cadhead <nekorides@gmail.com>
2022-03-24 14:20:02 +0300

    feat(index page): pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,33 @@ import Layout from "components/layout";
 import Container from "components/container";
 import { getAllPosts } from "lib/getPosts";
 import Head from "next/head";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import Posts from "components/posts";
 import Nav from "components/nav";
+import ReactPaginate from "react-paginate";
+import { ArrowCircleLeftIcon, ArrowCircleRightIcon, DotsHorizontalIcon } from "@heroicons/react/solid"
 
 type Props = {
   posts: IPost[]
 }
 
 export default function Home({ posts }: Props): ReactElement {
+  const [currentPosts, setCurrentPosts] = useState([] as IPost[]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentPosts(posts.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(posts.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, posts]);
+
+  const handlePageClick = (event: any) => {
+    const newOffset = (event.selected * itemsPerPage) % posts.length;
+    setItemOffset(newOffset);
+  };
+
   return (
     <Layout>
       <Head>
@@ -18,7 +36,18 @@ export default function Home({ posts }: Props): ReactElement {
       </Head>
       <Container>
         <Nav />
-        {posts.length ? <Posts posts={posts} /> : "No posts, yet."}
+        {posts.length ? <Posts posts={currentPosts} /> : "No posts, yet."}
+        <ReactPaginate
+          className="flex justify-center mt-5"
+          breakLabel={<DotsHorizontalIcon className="w-5 h-5 m-2 mt-2" />}
+          nextLabel={<ArrowCircleRightIcon className="w-5 h-5 m-2 mt-2" />}
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={4}
+          pageCount={pageCount}
+          previousLabel={<ArrowCircleLeftIcon className="w-5 h-5 m-2 mt-2" />}
+          pageLinkClassName="block m-2 px-2 bg-gray-200 rounded hover:bg-gray-100"
+          activeLinkClassName="text-indigo-600"
+        />
       </Container>
     </Layout>
   )
